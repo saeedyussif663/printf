55fa16f9a57b4a65f29ccf7b182f44636e656baf
saeedyussif663 <saeedyussif663@gmail.com>
2023-11-17 01:48:53 -0800

    print bin variables

--- print_binary.c
+++ print_binary.c
@@ -8,27 +8,27 @@
 int print_binary(va_list val)
 {
 int flag = 0;
-int cont = 0;
+int counter = 0;
 int i, a = 1, b;
-unsigned int num = va_arg(val, unsigned int);
+unsigned int number = va_arg(val, unsigned int);
 unsigned int p;
 
 for (i = 0; i < 32; i++)
 {
-p = ((a << (31 - i)) & num);
+p = ((a << (31 - i)) & number);
 if (p >> (31 - i))
 flag = 1;
 if (flag)
 {
 b = p >> (31 - i);
 _putchar(b + 48);
-cont++;
+counter++;
 }
 }
-if (cont == 0)
+if (counter == 0)
 {
-cont++;
+counter++;
 _putchar('0');
 }
-return (cont);
+return (counter);
 }
